edbe7fcc8aaef42949148595f4a01d646a5578b3
once2go <denys.karpov@petcube.com>
2016-08-02 20:40:05 +0300

    Add popular movie fetching usecase

---
+++ domain/src/main/java/com/once2go/domain/interactors/GetPopularMoviesUseCase.java
@@ -0,0 +1,21 @@
+package com.once2go.domain.interactors;
+
+import com.once2go.model.movies.Movie;
+
+import java.util.List;
+
+import rx.Observable;
+
+/**
+ * Created by once2go on 02.08.16.
+ */
+public class GetPopularMoviesUseCase extends UseCase<List<Movie>> {
+
+    public GetPopularMoviesUseCase() {
+    }
+
+    @Override
+    protected Observable<List<Movie>> buildUseCaseObservable() {
+        return serverApi.getPopularMovies();
+    }
+}
--- model/src/main/java/com/once2go/model/network/ServerApi.java
+++ model/src/main/java/com/once2go/model/network/ServerApi.java
@@ -1,7 +1,18 @@
 package com.once2go.model.network;
 
+import com.once2go.model.movies.Movie;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import rx.Observable;
+
 /**
  * Created by once2go on 01.08.16.
  */
-public class ServerApi {
+public interface ServerApi {
+
+    @GET("movies/popular")
+    Observable<List<Movie>> getPopularMovies();
+
 }

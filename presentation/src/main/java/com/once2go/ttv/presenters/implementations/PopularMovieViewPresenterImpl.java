fc903d82c45d0276accda7ea184f1f115fe7b470
once2go <denys.karpov@petcube.com>
2016-08-02 20:44:34 +0300

    Add data loading realisation via usecase

--- presentation/src/main/java/com/once2go/ttv/presenters/implementations/PopularMovieViewPresenterImpl.java
+++ presentation/src/main/java/com/once2go/ttv/presenters/implementations/PopularMovieViewPresenterImpl.java
@@ -2,22 +2,53 @@ package com.once2go.ttv.presenters.implementations;
 
 import android.support.annotation.NonNull;
 
+import com.once2go.domain.interactors.GetPopularMoviesUseCase;
+import com.once2go.model.movies.Movie;
 import com.once2go.ttv.presenters.PopularMovieViewPresenter;
 import com.once2go.ttv.views.PopularMovieView;
+
+import java.util.List;
+
+import rx.Subscriber;
 
 /**
  * Created by once2go on 02.08.16.
  */
 public class PopularMovieViewPresenterImpl implements PopularMovieViewPresenter {
 
+    private PopularMovieView mPopularMovieView;
+    private GetPopularMoviesUseCase mGetPopularMoviesUseCase;
+
+    private Subscriber<List<Movie>> mPopularMovieSubscription = new Subscriber<List<Movie>>() {
+        @Override
+        public void onCompleted() {
+
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            if (mPopularMovieView != null) {
+                mPopularMovieView.showError(e.getMessage());
+            }
+        }
+
+        @Override
+        public void onNext(List<Movie> movieList) {
+            if (mPopularMovieView != null) {
+                mPopularMovieView.onMovieListLoaded(movieList);
+            }
+        }
+    };
+
     @Override
     public void onLoadPopularMovie() {
-
+        mGetPopularMoviesUseCase = new GetPopularMoviesUseCase();
+        mGetPopularMoviesUseCase.execute(mPopularMovieSubscription);
     }
 
     @Override
     public void setView(@NonNull PopularMovieView view) {
-
+        mPopularMovieView = view;
     }
 
     @Override
@@ -32,6 +63,10 @@ public class PopularMovieViewPresenterImpl implements PopularMovieViewPresenter 
 
     @Override
     public void destroy() {
-
+        if (mGetPopularMoviesUseCase != null) {
+            mGetPopularMoviesUseCase.unsubscribe();
+            mGetPopularMoviesUseCase = null;
+        }
+        mPopularMovieView = null;
     }
 }
